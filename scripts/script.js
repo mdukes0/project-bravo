1a0b33ed9a2db0425127c0e508d3c62eb63d4de3
mdukes0 <mdukes@una.edu>
2025-10-06 18:12:07 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,11 @@
-// $( function() {
-//     $( "#draggable" ).draggable();
-//     $( "#droppable" ).droppable({
-//       drop: function( event, ui ) {
-//         $( this )
-//           .addClass( "ui-state-highlight" )
-//           .find( "p" )
-//             .html( "Dropped!" );
-//       }
-//     });
-//   } );
+$(function() {
+  $('.drag').draggable();
 
-$('.drag').draggable(); 
-
-//$ == jquery land.
-$( ".drop" ).droppable( 
-         { 
-            // over: function (event, ui) { 
-            //     console.log($(this).attr("id")); //the 'this' under over event
-            //   },
-            
-            drop: function (event, ui) {
-                alert(ui.draggable.attr('id'))
-             }
-    //what did i drag
-    
-    
-    console.log($(this).attr("class")); //TODO: fix
-    //where did i drop it
-    // console.log($(this).attr("id"));
-
-            //   $( this ).addClass( "redClass" );
-      //todo: if it's correct, make it green. 
-        
+  $(".drop").droppable({
+    drop: function (event, ui) {
+      const draggedGrid = ui.draggable.attr('id');
+      alert(draggedGrid);
+      console.log($(this).attr("class"));
     }
-);
+  });
+});
