6ba7fa55c86e55edbee63097691b82d6c4ab54a4
mdukes0 <mdukes@una.edu>
2025-10-08 15:17:20 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,37 @@ $(function() {
 
   $(".drop").droppable({
     drop: function (event, ui) {
-      const draggedGrid = ui.draggable.attr('id');
-      alert(draggedGrid);
-      console.log($(this).attr("class"));
+      // const draggedGrid = ui.draggable.attr('id');
+      // alert(draggedGrid);
+      // console.log($(this).attr("class"));
+
+      // $( this )
+      //     .addClass( "droppable" )
+      //     .html( "Dropped!" );
+
+      $('#straw-grid').draggable({
+        revert: function() {
+          if ($(this).hasId('#straw-basket')) {
+            $('#straw-basket').setAttribute("style", "background: green"); 
+            // return true; //return cell light up into green
+          } else if {
+            $(this).draggable({revert: invalid});
+          }//then just say if else revert: 'invalid'
+      }
+      });
+      
     }
   });
+
+//   if ($("#straw-grid").val() != '') {
+//     $(".draggable").draggable({revert: invalid});
+// }
+// else if($("#seats").val() < 99999) {
+//     alert("Not a valid Number");
+// } else {
+//     setflag = true;
+// }
+
+//https://stackoverflow.com/questions/3088485/how-to-revert-position-of-a-jquery-ui-draggable-based-on-condition
+//https://stackoverflow.com/questions/4520042/if-else-else-if-in-jquery-for-a-condition
 });
