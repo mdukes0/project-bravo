98160f663485324d12241592c525a483970341b2
mdukes0 <mdukes@una.edu>
2025-10-08 23:33:37 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,98 @@
-$(function() {
+
+$(function () {
   $('.drag').draggable();
 
-  $(".drop").droppable({
+  $('.drop').droppable({
+
     drop: function (event, ui) {
-      // const draggedGrid = ui.draggable.attr('id');
-      // alert(draggedGrid);
-      // console.log($(this).attr("class"));
+      const basketId = this.id;
+      const berryId = ui.draggable.attr('id');
 
-      // $( this )
-      //     .addClass( "droppable" )
-      //     .html( "Dropped!" );
+      if(
+        (berryId === 'straw-grid' && basketId === 'straw-basket') ||
+        (berryId === 'black-grid' && basketId === 'black-basket') ||
+        (berryId === 'blue-grid' && basketId === 'blue-basket') ||
+        (berryId === 'goose-grid' && basketId === 'goose-basket') 
+      ) {
 
-      $('#straw-grid').draggable({
-        revert: function() {
-          if ($(this).hasId('#straw-basket')) {
-            $('#straw-basket').setAttribute("style", "background: green"); 
-            // return true; //return cell light up into green
-          } else if {
-            $(this).draggable({revert: invalid});
-          }//then just say if else revert: 'invalid'
+        console.log('Correct berry placement');
+        alert('correct');
+
+
+        $(this).css('background', 'green');
+
+        ui.draggable.appendTo(this).css({
+          position: 'relative', top: 0, left: 0
+        }).draggable('disable');
+      } else {
+        console.log('Incorrect berry placement');
+        alert('Incorrect');
       }
-      });
-      
     }
   });
+});
+// $(function () {
+//   // Drag and revert back to position
+//   $('.drag').draggable({
+//     revert: function() {
+//       return !$(this).data('correct');
+//     }, // had to fix because other line only reverted
+//     //   return !$(this).data('correct');
+//     // },
+//     // start: function () {
+//     //   $(this).data('correct', false);
+//     start: function () {
+//       $(this).data('correct', false);
+//     }
+//   });
+
+//   // Hardcoded targets for each berry
+//   $('.drop').droppable({
+//     drop: function(event, ui) {
+//       const basketId = $(this).attr('id');
+//       const berryId = ui.draggable.attr('id');
+
+//   // Hardcoded to ensure that conditions react to specifc placements
+//   let placedIn = false;
+//   if(berryId === 'straw-grid' && basketId === 'straw-basket') placedIn = true;
+//   if (berryId === 'black-grid' && basketId === 'black-basket') placedIn = true;
+//   if (berryId === 'blue-grid' && basketId === 'blue-basket') placedIn = true;
+//   if (berryId === 'goose-grid' && basketId === 'goose-basket') placedIn = true;
+
+//   if (placedIn) {
+//     $(this).css('background', 'green');
+//     ui.draggable.data('correct', true);
+//     ui.draggable.draggable('disable');
+//   }
+// }
+  
+//   });
+// });
+
+
+  // $('.drag').draggable();
+
+  // $(".drop").droppable({
+  //   drop: function (event, ui) {
+  //     // const draggedGrid = ui.draggable.attr('id');
+  //     // alert(draggedGrid);
+  //     // console.log($(this).attr("class"));
+
+  //     $('#straw-grid').draggable({
+  //       revert: function () {
+  //         if (!$(this).data('correct')) {
+  //           return true;
+  //         }
+  //       },
+  //       start: function () {
+  //         $(this).data('correct', false);
+  //       }
+  //     });
+
+
+
+  //   }
+  // });
 
 //   if ($("#straw-grid").val() != '') {
 //     $(".draggable").draggable({revert: invalid});
@@ -36,4 +105,3 @@ $(function() {
 
 //https://stackoverflow.com/questions/3088485/how-to-revert-position-of-a-jquery-ui-draggable-based-on-condition
 //https://stackoverflow.com/questions/4520042/if-else-else-if-in-jquery-for-a-condition
-});
